1d5bd489cdba226a220dd82693ef761fb741f299
arieldiaz11777 <arieldiazapp1177@gmail.com>
2024-10-25 15:01:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 const html5QrCode = new Html5Qrcode("reader");
 
-document.getElementById("start-button").addEventListener("click", function () {
-    // Iniciar el escaneo
+document.getElementById("start-button").addEventListener("click", function() {
     html5QrCode.start(
-        { facingMode: "environment" }, // Usa la cámara trasera en dispositivos móviles
+        { facingMode: "environment" },
         {
             fps: 10,
-            qrbox: 250 // Tamaño del área de escaneo
+            qrbox: 250
         },
         (decodedText, decodedResult) => {
-            // Este callback se ejecuta cuando se detecta un código QR
             console.log(`Código QR escaneado: ${decodedText}`);
-            // Redirigir a la URL del código QR
             window.location.href = decodedText;
-
-            // Después de redirigir, reiniciar el escáner después de un tiempo
             setTimeout(() => {
-                // Detener el escáner
                 html5QrCode.stop().then(ignore => {
                     console.log("Escaneo detenido");
                 }).catch(err => {
                     console.error("Error al detener el escáner", err);
                 });
-
-                // Reiniciar la página
                 window.location.reload();
-            }, 5000); // Esperar 5 segundos antes de reiniciar
+            }, 5000);
         },
         (errorMessage) => {
-            // Este callback se ejecuta cuando hay un error en el escaneo
             console.error("Error de escaneo", errorMessage);
         }
     ).catch(err => {
